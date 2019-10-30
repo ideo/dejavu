3d4b51dd0e93f65aa2fa2d2452d66dacd2975dd1
maniart <mani.art@gmail.com>
2019-10-30 14:14:02 -0400

    WIP

---
+++ google.js
@@ -0,0 +1,250 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+
+// temp. this should come from Slack App.
+let topic = "future";
+
+function createDocNameForTopic(_topic) {
+  return `Dejavu Insights - ${_topic}`;
+}
+
+// If modifying these scopes, delete token.json.
+const SCOPES = [
+  "https://www.googleapis.com/auth/drive",
+  "https://www.googleapis.com/auth/documents"
+];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
+
+/**
+ * Search in IDEO G-Drive for a given topic
+ * @param {string} topic The topic to search for.
+ */
+function search(auth, topic) {
+  const drive = google.drive({ version: "v3", auth });
+  const docs = google.docs({ version: "v1", auth });
+  const query = `mimeType = 'application/vnd.google-apps.document' and name contains '${topic}'`;
+
+  drive.files.list(
+    {
+      q: query,
+      pageSize: 10,
+      fields:
+        "nextPageToken, files(id, name, lastModifyingUser, webViewLink, modifiedTime)"
+    },
+    (err, res) => {
+      // Error Handling
+      if (err) {
+        return console.log(
+          "#search: Searching for topic failed. The API returned an error: " +
+            err
+        );
+      }
+
+      // Matching files
+      const files = res.data.files;
+
+      if (files.length) {
+        files.map(file => {
+          console.log(`#search: Found a match: ${file.name} (${file.id})`);
+          return docs.documents.get(
+            {
+              documentId: file.id
+            },
+            (err, res) => {
+              if (err) {
+                return console.log(
+                  "#search: Error while digging into a matchinc file: " + err
+                );
+              }
+              let result = "";
+              res.data.body.content.forEach(block => {
+                if (block.paragraph && block.paragraph.elements) {
+                  block.paragraph.elements.forEach(element => {
+                    if (
+                      element.textRun &&
+                      element.textRun.content &&
+                      typeof element.textRun.content === "string" &&
+                      element.textRun.content.includes(topic)
+                    ) {
+                      result = result.concat("\n");
+                      result = result.concat(element.textRun.content);
+                    }
+                  });
+                }
+              });
+
+              console.log(
+                "#search found the following relevant paragraphs in relevant docs:\n"
+              );
+              console.log(result);
+              console.log("\n");
+            }
+          );
+        });
+      }
+    }
+  );
+}
+
+/**
+ * add an insight related to a topic in IDEO G-Drive
+ * @param {string} topic The topic to add content for.
+ * @param {string} content The research insight itself.
+ */
+function add(auth, topic, content) {
+  const drive = google.drive({ version: "v3", auth });
+  const docs = google.docs({ version: "v1", auth });
+
+  // Folder ID for Dejavu. This folder contains all research insights in IDEO Google Drive.
+  let folderId = null;
+  // 1. does the insight folder exist?
+  drive.files.list(
+    {
+      q: `mimeType = 'application/vnd.google-apps.folder' and name = 'dejavu'`,
+      fields: "files(id, name)"
+    },
+    (err, res) => {
+      if (err) {
+        // error handling
+        return console.log(
+          "#add: while looking for `dejavu` folder, the API returned an error: ",
+          err
+        );
+      }
+
+      if (res.data.files.length) {
+        console.log("#add: dejavu folder exists: ", res.data.files[0].id);
+        // folder exists. let's make a doc
+        // keep the reference to the folder
+        folderId = res.data.files[0].id;
+      } else {
+        // folder does not exist. let's make it
+        const fileMetadata = {
+          name: "dejavu",
+          mimeType: "application/vnd.google-apps.folder"
+        };
+        drive.files.create(
+          {
+            resource: fileMetadata,
+            fields: "id"
+          },
+          (err, file) => {
+            if (err) {
+              // Handle error
+              console.error(
+                "#add: error while trying to create `dejavu` folder: ",
+                err
+              );
+            } else {
+              console.log("#add: made `dejavu` Folder: ", file.data.id);
+              // keep the reference to the folder
+              folderId = file.data.id;
+            }
+          }
+        );
+      }
+
+      drive.files.create(
+        {
+          resource: {
+            name: `dejavu-insights-${topic}`,
+            parents: [folderId]
+          }
+        },
+        (err, file) => {
+          if (err) {
+            return console.log(
+              "#add: failed to create the documnt ",
+              `dejavu-insights-${topic} `,
+              err
+            );
+          }
+          console.log(
+            "#add: successfully created insight document: ",
+            `dejavu-insights-${topic}`,
+            " and file id is: ",
+            file.data.id
+          );
+        }
+      );
+    }
+  );
+}
+
+/**
+ * HOF that returns a function which will authorize
+ * and perform the provided callback once authorized.
+ * @param {function} APICall The callback to call with the authorized client.
+ */
+function authorizeAndMakeAPICall(APICall) {
+  return function makeApiCall(argsArray) {
+    // Load client secrets from a local file.
+    fs.readFile("credentials.json", (err, content) => {
+      if (err) return console.log("Error loading credentials.json:", err);
+      // Authorize a client with credentials, then call the Google Drive API.
+      authorize(JSON.parse(content), APICall, argsArray);
+    });
+  };
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback, callbackArgsArray) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, ...callbackArgsArray);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("Enter the code from that page here: ", code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error retrieving access token", err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+module.exports = {
+  add: authorizeAndMakeAPICall(add),
+  search: authorizeAndMakeAPICall(search)
+};
---
+++ index.js
@@ -0,0 +1,477 @@
+// Botkit's core features
+const { Botkit } = require("botkit");
+// Botkit CMS integration
+const { BotkitCMSHelper } = require("botkit-plugin-cms");
+// Fetch
+const fetch = require("node-fetch");
+// Import data for Slack blocks
+const { insightsCollectionTemplate } = require("./data.json");
+
+// Import a platform-specific adapter for slack.
+const {
+  SlackAdapter,
+  SlackMessageTypeMiddleware,
+  SlackEventMiddleware
+} = require("botbuilder-adapter-slack");
+// MongoDB to store conversations
+const { MongoDbStorage } = require("botbuilder-storage-mongodb");
+
+// Load process.env values from .env file
+require("dotenv").config();
+
+let storage = null;
+if (process.env.MONGO_URI) {
+  storage = new MongoDbStorage({
+    url: process.env.MONGO_URI,
+    database: "dejavu",
+    collection: "messages"
+  });
+}
+
+const adapter = new SlackAdapter({
+  // parameters used to secure webhook endpoint
+  verificationToken: process.env.verificationToken,
+  clientSigningSecret: process.env.clientSigningSecret,
+
+  // auth token for a single-team app
+  botToken: process.env.botToken,
+
+  // credentials used to set up oauth for multi-team apps
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  scopes: ["bot", "client"],
+  redirectUri: process.env.redirectUri,
+
+  // functions required for retrieving team-specific info
+  // for use in multi-team apps
+  getTokenForTeam: getTokenForTeam,
+  getBotUserByTeam: getBotUserByTeam
+});
+
+// Use SlackEventMiddleware to emit events that match their original Slack event types
+adapter.use(new SlackEventMiddleware());
+
+// Use SlackMessageType middleware to further classify messages as direct_message, direct_mention, or mention
+adapter.use(new SlackMessageTypeMiddleware());
+
+const controller = new Botkit({
+  webhook_uri: "/api/messages",
+  adapter: adapter,
+  storage
+});
+
+if (process.env.cms_uri) {
+  controller.usePlugin(
+    new BotkitCMSHelper({
+      cms_uri: process.env.cms_uri,
+      token: process.env.cms_token
+    })
+  );
+}
+
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + "/features");
+
+  // catch-all that uses the CMS to trigger dialogs
+  if (controller.plugins.cms) {
+    controller.on("message, direct_message", async (bot, message) => {
+      let results = false;
+      try {
+        results = await controller.plugins.cms.testTrigger(bot, message);
+      } catch (e) {
+        console.error("> Failed at `#controller.plugins.cms.testTrigger`", e);
+      }
+
+      if (results !== false) {
+        // do not continue middleware!
+        return false;
+      }
+    });
+  }
+});
+
+const KNOWN_VERBS = ["add", "search"];
+const ACTIONS = {
+  BLOCK_ACTIONS: "block_actions",
+  VIEW_SUBMISSION: "view_submission",
+  VIEW_CLOSED: "view_closed"
+};
+
+// Keep reference to the latest `topic.`
+// This mutates everytime user types in `/dejavu add [topic]`
+let topic = "";
+
+// for modal.open payloads, we do get a responseURL but for modal submissions we don't.
+// to respond with a message in respons to form submission, we hold onto the responseURL here.
+let cachedResponseUrl = null;
+
+function createInsightsCollectionForm(collectionTemplate, topic) {
+  const form = Object.assign({}, collectionTemplate);
+  form.blocks[0].elements[0].text = `Topic: ${topic}`;
+  return form;
+}
+
+function sendMessageToSlackResponseURL(responseURL, JSONMessage, token) {
+  return fetch(responseURL, {
+    method: "POST",
+    body: JSON.stringify(JSONMessage),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(res => {
+      if (res.status === 200) {
+        return true;
+      } else {
+        throw new Error(
+          "> Message sent to slack, but it came back with a non-200 response: " +
+            JSON.stringify(res)
+        );
+      }
+    })
+    .then(() => {
+      console.log("> Successfully sent message to Slack Response URL.");
+    })
+    .catch(e => {
+      console.log("> Woops!", e);
+    });
+}
+
+/* 
+  The following endpoing processes every slash command  ala `/dejavu search amex`
+*/
+controller.webserver.post("/api/slash-commands", (req, res, next) => {
+  // Collect datapoints of interest & create some consts.
+  const {
+    body: {
+      text: commandText,
+      user_name: userName,
+      response_url: responseUrl,
+      trigger_id: triggerID,
+      token
+    }
+  } = req;
+
+  // Immediately respond to Slack
+  if (process.env.verificationToken !== token) {
+    // Unauthorozied
+    res.status(403).end("Access Forbidden");
+  } else {
+    // Best practice to respond with empty 200
+    res.status(200).end();
+  }
+
+  // Log what we got!
+  console.log(
+    "\nText: ",
+    commandText,
+    "\nUser Name: ",
+    userName,
+    "\nResponse URL: ",
+    responseUrl,
+    "\nTrigger ID: ",
+    triggerID
+  );
+
+  // The verb entered by the user
+  const verb = commandText.split(" ").shift();
+
+  // Decide what the appropriate response should be
+  if (!KNOWN_VERBS.includes(verb)) {
+    // Push the response to Slack.
+    fetch(responseUrl, {
+    method: "POST",
+    body: JSON.stringify({
+      "response_type": "ephemeral",
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Woops, my bad. I can only understand the following tasks: add, search like so: \n \`/dejavu [add, search] [topic or keyword]\``
+          }
+        }
+      ]}),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return true;
+      } else {
+        throw new Error(
+          "> Message sent to slack, but it came back with a non-200 response: " +
+            JSON.stringify(res)
+        );
+      }
+    })
+    .then(() => {
+      console.log("> Successfully sent message to Slack Response URL.");
+    })
+    .catch(e => {
+      console.log("> Woops!", e);
+    });
+  } else {
+    // We know what the user meant. So we continue with the interaction flow.
+    const [, ...rest] = commandText.split(" ");
+    // The user has declared their topic of interest. Hold onto it.
+    topic = rest.join(" ");
+    // Push the response to Slack.
+    fetch(responseUrl, {
+    method: "POST",
+    body: JSON.stringify({
+      "response_type": "ephemeral",
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Great! From what I understand you want to ${verb === "add" ? "add to" : "search for"} insights related to ${rest.join(" ")}. Is that correct?`
+          }
+        }, {
+        "type": "actions",
+        "block_id": "dejavu-intent-confirmation-buttons-block",
+        "elements": [
+          {
+            "type": "button",
+            "value": "true",
+            "style": "primary",
+            "action_id": "dejavu-intent-confirmation-buttons-true",
+            "text": {
+              "type": "plain_text",
+              "text": "Yep!"
+            }
+          },
+          {
+            "type": "button",
+            "value": "false",
+            "style": "danger",
+            "action_id": "dejavu-intent-confirmation-buttons-false",
+            "text": {
+              "type": "plain_text",
+              "text": "Nope."
+            }
+          }
+        ]}
+      ] 
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(res => {
+      if (res.status === 200) {
+        return true;
+      } else {
+        throw new Error(
+          "> Message sent to slack, but it came back with a non-200 response: " +
+            JSON.stringify(res)
+        );
+      }
+    })
+    .then(() => {
+      console.log("> Successfully sent message to Slack Response URL.");
+    })
+    .catch(e => {
+      console.log("> Woops!", e);
+    });
+  }
+
+  return next();
+});
+
+/* 
+  The following endpoing processes modal interactions
+*/
+controller.webserver.post("/api/interactions", (req, res, next) => {
+  // Best practice to respond with empty 200
+  res.status(200).end();
+
+  const {
+    body: { payload }
+  } = req;
+  const parsedPayload = JSON.parse(payload);
+  const { type, response_url: responseUrl } = parsedPayload;
+
+  if (responseUrl) {
+    cachedResponseUrl = responseUrl;
+  }
+
+  // An action invoked by an interactive component
+  if (type === ACTIONS.BLOCK_ACTIONS) {
+    const { actions, trigger_id: triggerId } = parsedPayload;
+    const [{ value }] = actions;
+    if (value === "true") {
+      // User clicked on 'Yep' button
+      fetch("https://slack.com/api/views.open", {
+        method: "POST",
+        headers: {  
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.botToken}`
+        },
+        body: JSON.stringify({
+          trigger_id: triggerId,
+          view: JSON.stringify(
+            createInsightsCollectionForm(insightsCollectionTemplate, topic)
+          )
+        })
+      })
+        .then(res => res.json())
+        .then(parsedResponse => {
+          if (parsedResponse.ok) {
+            console.log("> Dejavu: successfully opened modal");
+          } else {
+            const responseBody = {
+              response_type: "ephemeral",
+              blocks: [
+                {
+                  type: "section",
+                  text: {
+                    type: "plain_text",
+                    text:
+                      "Oops. Something is amiss. I have notified my developer to resolve this issue ASAP. Sorry about that!"
+                  }
+                }
+              ]
+            };
+
+            // Push the response to Slack.
+            sendMessageToSlackResponseURL(
+              cachedResponseUrl,
+              responseBody,
+              process.env.botToken
+            );
+            throw new Error(parsedResponse.error);
+          }
+        })
+        .catch(e => console.log("Woops. ", e));
+    } else if (value === "false") {
+      // User clicked on 'Nope' button
+      const responseBody = {
+        response_type: "ephemeral",
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `Gotcha. Feel free to call me again anytime like so: \`/dejavu [add, search] [topic or keyword]\``
+            }
+          }
+        ]
+      };
+
+      // Push the response to Slack.
+      sendMessageToSlackResponseURL(
+        cachedResponseUrl,
+        responseBody,
+        process.env.botToken
+      );
+    }
+  } else if (type === ACTIONS.VIEW_SUBMISSION) {
+    // A Modal submission happened
+    const data = Object.values(parsedPayload.view.state.values);
+    console.log(
+      "\n -->We got a submission!",
+      Object.values(parsedPayload.view.state.values),
+      "---> topic is: ",
+      topic,
+      "\n"
+    );
+
+    const responseBody = {
+      response_type: "ephemeral",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Great! You research insight on ${topic} is being saved. I'll give you a link shortly...`
+          }
+        }
+      ]
+    };
+    console.log("Insight recorded response body: ", responseUrl, responseBody);
+    // Push the response to Slack.
+    sendMessageToSlackResponseURL(
+      cachedResponseUrl,
+      responseBody,
+      process.env.botToken
+    );
+
+    topic = ""; // reset the topic
+  } else if (type === ACTIONS.VIEW_CLOSED) {
+    console.log("View Closed");
+  }
+
+  // return next();
+});
+
+controller.webserver.get("/", (req, res) => {
+  res.send(`This app is running Botkit ${controller.version}.`);
+});
+
+controller.webserver.get("/install", (req, res) => {
+  // getInstallLink points to slack's oauth endpoint and includes clientId and scopes
+  res.redirect(controller.adapter.getInstallLink());
+});
+
+controller.webserver.get("/install/auth", async (req, res) => {
+  try {
+    const results = await controller.adapter.validateOauthCode(req.query.code);
+
+    console.log("FULL OAUTH DETAILS", results);
+
+    // Store token by team in bot state.
+    tokenCache[results.team_id] = results.bot.bot_access_token;
+
+    // Capture team to bot id
+    userCache[results.team_id] = results.bot.bot_user_id;
+
+    res.json("Success! Bot installed.");
+  } catch (err) {
+    console.error("OAUTH ERROR:", err);
+    res.status(401);
+    res.send(err.message);
+  }
+});
+
+let tokenCache = {};
+let userCache = {};
+
+if (process.env.TOKENS) {
+  tokenCache = JSON.parse(process.env.TOKENS);
+}
+
+if (process.env.USERS) {
+  userCache = JSON.parse(process.env.USERS);
+}
+
+async function getTokenForTeam(teamId) {
+  if (tokenCache[teamId]) {
+    return new Promise(resolve => {
+      setTimeout(function() {
+        resolve(tokenCache[teamId]);
+      }, 150);
+    });
+  } else {
+    console.error("Team not found in tokenCache: ", teamId);
+  }
+}
+
+async function getBotUserByTeam(teamId) {
+  if (userCache[teamId]) {
+    return new Promise(resolve => {
+      setTimeout(function() {
+        resolve(userCache[teamId]);
+      }, 150);
+    });
+  } else {
+    console.error("Team not found in userCache: ", teamId);
+  }
+}
