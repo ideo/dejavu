ef9fc80b2eea947aa4be4ae3e1ad46146b1439a2
maniart <mani.art@gmail.com>
2019-10-31 12:29:03 -0400

    WIP

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-console.log('*___________*');
-console.log(process.env.GOOGLE_CONFIG);
-console.log('*___________*');
+// console.log('*___________*');
+// console.log(process.env.GOOGLE_CONFIG);
+// console.log('*___________*');
 
-try {
-  fs.writeFileSync(path.join(__dirname, '../google-credentials-heroku.json'), process.env.GOOGLE_CONFIG);
-} catch(e) {
-  console.log('________ error while writing google config to file', e);
-}
+// try {
+//   fs.writeFileSync(path.join(__dirname, '../google-credentials-heroku.json'), process.env.GOOGLE_CONFIG);
+// } catch(e) {
+//   console.log('________ error while writing google config to file', e);
+// }
