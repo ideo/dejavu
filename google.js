b4c2db36efb046fb41c394fc545927180ae15a1a
maniart <mani.art@gmail.com>
2019-11-01 14:48:33 -0400

    WIP

--- google.js
+++ google.js
@@ -83,7 +83,7 @@ async function runSample() {
             // console.log(JSON.stringify(element));
             console.log('_______ element.textRun', !!element.textRun);
             console.log('_______ element.textRun.content', !!element.textRun.content);
-            console.log('_______ typeof element.textRun.content === "string"', !!typeof element.textRun.content === "string");
+            console.log('_______ typeof element.textRun.content === "string"', !!typeof element.textRun.content === "string", typeof element.textRun.content);
             console.log('_______ element.textRun.content.includes(topic)', !!element.textRun.content.includes(topic));
             
             if (element.textRun && element.textRun.content && typeof element.textRun.content === "string" && element.textRun.content.includes(topic)) {
