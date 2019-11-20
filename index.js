ffc6e973a9567a9cc21f3319bf121f83b8a5ef53
maniart <mani.art@gmail.com>
2019-11-20 17:57:32 -0500

    WIP

--- index.js
+++ index.js
@@ -76,14 +76,18 @@ async function createInsightsCollectionForm(collectionTemplate, topic) {
       }
   */
   const form = Object.assign({}, collectionTemplate);
-  form.blocks[3].accessory.options = await getClientTags().map(doc => (
+  const clientTagsResource = await getClientTags()
+  const clientTags = clientTagsResource.map(doc => doc.data().tag)
+  console.log('clientTags: ', clientTags)
+
+  form.blocks[3].accessory.options = clientTags.map(tag => (
     {
       "text": {
         "type": "plain_text",
-        "text": doc.data().tag,
+        "text": tag,
         "emoji": true
       },
-      "value": doc.data().tag
+      "value": tag
     }
   ))
   form.blocks[0].elements[0].text = `Topic: ${topic}`;
