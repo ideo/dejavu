70462ba4ac47017f4011e404c05f48e3c16c84a3
maniart <mani.art@gmail.com>
2019-11-22 17:18:07 -0500

    WIP

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ controller.webserver.post('/api/interactions', (req, res, next) => {
 
     console.log(
       '\n -->We got a submission!',
-      Object.values(parsedPayload.view.state.values),
+      JSON.stringify(Object.values(parsedPayload.view.state.values)),
       '---> topic is: ',
       topic,
       '\n'
